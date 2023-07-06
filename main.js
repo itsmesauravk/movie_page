2c57ee19889ebad06ff06c72ff9b8639026015c9
itsmesauravk <sauravkarki10.12@gmail.com>
2023-07-06 21:16:01 +0545

    added website movie

---
+++ main.js
@@ -0,0 +1,41 @@
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+const movieResults = document.getElementById('movie-results');
+
+
+searchButton.addEventListener('click', () => {
+  movieResults.style.display = "flex";
+  const searchTerm = searchInput.value;
+  if (searchTerm !== '') {
+    fetchMovieData(searchTerm);
+  } else {
+    alert('Please enter a movie title');
+  }
+});
+
+async function fetchMovieData(searchTerm) {
+  const apiUrl = `http://www.omdbapi.com/?apikey=c8bd318d&s=${searchTerm}`;
+  const data = await fetch(apiUrl).then(response => response.json());
+  console.log(data)
+  if (data.Response === 'True') {
+    displayMovieResults(data.Search);
+  } else {
+    displayError(data.Error);
+  }
+}
+
+
+function displayMovieResults(results) {
+  movieResults.innerHTML = results.map(result => 
+    `<div class="logo">FAKEFLIX</div> 
+    <div class="movie">
+      <img src="${result.Poster}" alt="Movie Poster">
+      <h2>${result.Title}</h2>
+      <p>Year: ${result.Year}</p>
+      <p>${result.Type}</p>
+    </div> `);
+}
+
+function displayError(errorMessage) {
+  movieResults.innerHTML = `<p class="error">${errorMessage}</p>`;
+}
